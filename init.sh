6be1968aec78de7eb32ba61ad183c1993afcd006
denis <denis.kisel92@gmail.com>
2018-06-18 13:54:49 +0300

    fix 2.0.x - 2.1.x. add condition yes/no

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+read -p "Are you sure? [y/n]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
 source conf.sh
 
 DIR_ROOT=$(pwd)
